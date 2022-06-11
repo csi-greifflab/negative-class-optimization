29189f056da45e731901aeb3c713eeda5945e645
ursueugen <ursu_eugen@hotmail.com>
2022-06-12 00:11:19 +0300

    1_vs_all and struct similarity

--- manage.sh
+++ manage.sh
@@ -8,6 +8,8 @@ if [[ $COMMAND == "update_env" ]]; then
     echo "Running update_env"
     # conda env create --force --file environment.yml
     conda env update --file environment.yml --prune
+    echo "Installing local immuneML in [--editable] mode"
+    pip install -e immuneML/immuneML
 else
     echo "Command $COMMAND not detected."
 fi
