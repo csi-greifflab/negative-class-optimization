e16010d7e0d191331937b9661ba4d86027214f6d
ursueugen <ursu_eugen@hotmail.com>
2022-06-13 18:22:23 +0300

    Minor bug in manage.sh

--- manage.sh
+++ manage.sh
@@ -8,8 +8,11 @@ if [[ $COMMAND == "update_env" ]]; then
     echo "Running update_env"
     # conda env create --force --file environment.yml
     conda env update --file environment.yml --prune
-    echo "Installing local immuneML in [--editable] mode"
-    pip install -e immuneML/immuneML
+    # echo "Installing local immuneML in [--editable] mode"
+    # pip install -e immuneML/immuneML
+elif [[ $COMMAND == "install_env" ]]; then
+    echo "Running install_env"
+    conda env create --force --file environment.yml
 else
     echo "Command $COMMAND not detected."
 fi
