3b29e14f29fc2f871def9be7586400ab1e066727
ursueugen <ursu_eugen@hotmail.com>
2023-03-31 19:41:04 +0300

    Rerun 12a for linear models for some specific runs that had a irrelevant runtime non-code error.

--- scripts/script_12a_train_SN10_clean.py
+++ scripts/script_12a_train_SN10_clean.py
@@ -3,30 +3,24 @@
 
 
 import itertools
+import math
 import multiprocessing
 from pathlib import Path
 from typing import List
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
-import math
-
-
-import mlflow
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
-from NegativeClassOptimization import ml, pipelines
-from NegativeClassOptimization import utils
-from NegativeClassOptimization import preprocessing
-from NegativeClassOptimization import config
-
+import mlflow
+from NegativeClassOptimization import config, ml, pipelines, preprocessing, utils
 
 TEST = False
-RESTRICTED_AG_COMBINATIONS = False
+RESTRICTED_AG_COMBINATIONS = True
 
 experiment_id = 11
 run_name = "dev-v0.1.2-3-with-replicates-linear"
@@ -35,8 +29,8 @@ num_processes = 20
 load_from_miniabsolut = True
 shuffle_antigen_labels = False
 swa = True
-seed_id = [0, 1, 2, 3]  # default was 0
-load_from_miniabsolut_split_seeds = [0, 1, 2, 3, 4]  # default None --(internally)--> 42
+seed_id = [3]  # default was 0
+load_from_miniabsolut_split_seeds = []  # default None --(internally)--> 42
 model_type = "LogisticRegression"
 
 epochs = 50
@@ -50,22 +44,20 @@ sample_train = None
 
 
 def multiprocessing_wrapper_script_12a(
-    experiment_id, 
+    experiment_id,
     run_name,
     ag_pos,
     ag_neg,
     sample_train,
     seed_id,
     load_from_miniabsolut_split_seed,
-    ):
-    
+):
     with mlflow.start_run(
-        experiment_id=experiment_id, 
-        run_name=run_name, 
+        experiment_id=experiment_id,
+        run_name=run_name,
         description=f"{ag_pos} vs {ag_neg}",
         tags={"mlflow.runName": run_name},
-        ):
-
+    ):
         pipe = pipelines.BinaryclassPipeline(
             log_mlflow=True,
             save_model_mlflow=True,
@@ -96,42 +88,53 @@ def multiprocessing_wrapper_script_12a(
 
 
 if __name__ == "__main__":
-
     utils.nco_seed()
     mlflow.set_tracking_uri(config.MLFLOW_TRACKING_URI)
-    experiment = mlflow.set_experiment(experiment_id=experiment_id)
+    experiment = mlflow.set_experiment(experiment_id=experiment_id)  # type: ignore
 
     antigens: List[str] = config.ANTIGENS
     ag_perms = list(itertools.permutations(antigens, 2))
 
     if RESTRICTED_AG_COMBINATIONS:
-        ag_perms = list(filter(lambda x: x[0] == "1ADQ", ag_perms))
+        # ag_perms = list(filter(lambda x: x[0] == "1ADQ", ag_perms))
+        ag_perms = [
+            ("1H0D", "1NSN"),
+            ("3RAJ", "1OB1"),
+            ("1H0D", "3VRL"),
+            ("5E94", "1NSN"),
+            ("5E94", "1OB1"),
+            ("5E94", "1ADQ"),
+            ("5E94", "1FBI"),
+            ("3RAJ", "1FBI"),
+            ("3RAJ", "1H0D"),
+            ("3RAJ", "5E94"),
+            ("3RAJ", "1WEJ"),
+        ]
 
     if TEST:
-
         epochs = 3
         learning_rate = 0.001
         optimizer_type = "Adam"
         momentum = 0.9
         weight_decay = 0
         batch_size = 64
-                
+
         multiprocessing_wrapper_script_12a(
             experiment_id,
             "test",
-            "3VRL", # ag_perms[0][0],
-            "1NSN", # ag_perms[0][1],
+            "3VRL",  # ag_perms[0][0],
+            "1NSN",  # ag_perms[0][1],
             None,
             0,
             None,
         )
-    
-    else:    
+
+    else:
         # Run batched multiprocessing
         for seed in seed_id:
             for i in range(0, len(ag_perms), num_processes):
                 print(f"Batch {i} of {len(ag_perms) / num_processes}")
-                ag_perms_batch = ag_perms[i:i+num_processes]
+                ag_perms_batch = ag_perms[i : i + num_processes]
                 with multiprocessing.Pool(processes=num_processes) as pool:
                     pool.starmap(
                         multiprocessing_wrapper_script_12a,
@@ -146,12 +149,12 @@ if __name__ == "__main__":
                                 None,
                             )
                             for ag_perm in ag_perms_batch
-                        ]
+                        ],
                     )
         for load_from_miniabsolut_split_seed in load_from_miniabsolut_split_seeds:
             for i in range(0, len(ag_perms), num_processes):
                 print(f"Batch {i} of {len(ag_perms) / num_processes}")
-                ag_perms_batch = ag_perms[i:i+num_processes]
+                ag_perms_batch = ag_perms[i : i + num_processes]
                 with multiprocessing.Pool(processes=num_processes) as pool:
                     pool.starmap(
                         multiprocessing_wrapper_script_12a,
@@ -166,5 +169,5 @@ if __name__ == "__main__":
                                 load_from_miniabsolut_split_seed,
                             )
                             for ag_perm in ag_perms_batch
-                        ]
+                        ],
                     )
