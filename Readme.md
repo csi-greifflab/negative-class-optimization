2875cc9585e287d38955d75adac62641b37c51b0
ursueugen <ursu_eugen@hotmail.com>
2024-07-14 15:06:04 +0300

    Cleaning the repo for repro

--- Readme.md
+++ Readme.md
@@ -1,56 +1,84 @@
-# Negative dataset optimization
+# Training data composition determines machine learning generalization and biological rule discovery
 
-![version badge](https://img.shields.io/badge/version-0.1.2-green)
+![version badge](https://img.shields.io/badge/version-0.9-green)
 
-This is the `Readme` for the `NegativeDatasetOptimization` project, internally abbreviated as `nco`. This is the closest representative term, as the scope of the project is dynamic. Follow this readme to setup everything and run.
+[Preprint](https://www.biorxiv.org/content/10.1101/2024.06.17.599333v2.abstract)
 
 ## Setup
 
-Once you've cloned the repository, run:
-
+### A. Environment
+Once you've cloned the repository, install the environment using conda based on `environment.yml` file and install the local package.
 ```
-bash manage.sh install_env
+conda env create --force --file environment.yml
 conda activate nco
-bash manage.sh update_env  # updates and installs local packages
-dvc pull  # fetches all the data
+pip install -e src/NegativeClassOptimization
 ```
 
-### Library dependencies
+### B. Data
 
-To add a new library dependence, please add it manually in the `environment.yml` file and run `bash manage.sh update_env`. Don't forget to commit and push.
-
-### Data
-
-#### DVC
-
-This will setup the environment and required data, including the `data` directory, with all required data.
-
-The `dvc pull` will fetch all the data, and at times this can be a lot. Often one will want to work with just a single file, one can achieve that with `dvc get` (usually not `dvc import`, to implement read-only mode, since we don't want changes to standard files). For convenience `./manage.sh get_700k` fetches the 700k dataset. We are also using `dvc` data pipelines. Please familiarize yourself with the API and check the file for the details.
-
-Once you've made changes to a `DVC`-tracked directory, please add the changes to `dvc` and `git` (provided example is for the `data` directory):
+### Absolut
+Download `Absolut` data. Get the doi csv from [data source](https://archive.norstore.no/pages/public/datasetDetail.jsf?id=10.11582/2021.00063), save it to data/Absolut/toc_doi10.11582_2021.00063.csv. Then run:
 
 ```
-dvc add data
-git commit data.dvc -m "data directory updates"
-dvc push
+python scripts/script_01_build_datasets.py download_absolut
 ```
 
-Note that to control file size, some large files are ignored (check `.dvcignore`).
+### Experimental data
 
-For more information check [DVC documentation](https://dvc.org/doc/start/data-management?tab=Mac-Linux).
+TODO
 
-#### Download `Absolut` data
-Get the doi csv from [data source](https://archive.norstore.no/pages/public/datasetDetail.jsf?id=10.11582/2021.00063), save it to data/Absolut/toc_doi10.11582_2021.00063.csv. Then run `python scripts/script_01_build_datasets.py download_absolut`.
 
-### mlflow
+## Precomputations
 
-We use `mlflow` to track experimental results, it is setup as a docker container. All details in `mlflow/` directory.
+Run the following scripts to run all the necessary precomputations for the further analyses.
 
-## Organization
+1. Generate the Miniabsolut dataset(s). Each MiniAbsolut dataset is a subset of the Absolut dataset with a specific number of samples per class for easier further assembly of training and test datasets, having already splitted training and test data, and removing duplicates and possible intersections. The script generates the datasets and saves them to `data/MiniAbsolut` and `data/MiniAbsolut_Splits`. Run:
 
-We organize most of the code in:
+```
+python scripts/script_01_build_datasets.py miniabsolut
+```
 
-1. Local library `src/NegativeClassOrganization` for functionality required everywhere else. Most organized and clean from this repo.
-2. `/notebooks` for exploratory and results analyses, local development, short experiments and others in this spirit. The messiest fom this repo. We try to keep notebooks short and move well-established functionality into local library and scripts.
-3. `/scripts` for the stable, established, reproducible analyses. These are tracked and managed through mlops tools like dvc and mlflow. They are cleaner than notebooks and leverage the local library. Check `dvc dag` for a dependency graph of the stages. **TODO:** add embed Google Sheets.
-4. Other, such as `mlflow/` for docker-based deployment of `mlflow` and auxiliary services.
+1. Train the various models on the various datasets. SN10 and Logistic Regression models are trained. Datasets: Absolut data, experimental data, shuffled controls, single epitope-only data, TODO. Results are saved at TODO (Frozen_...).
+
+```
+# SN10: Absolut
+script_12a_train_SN10_clean.py
+script_12c_train_SN10_clean_1v9.py
+script_12d_train_SN10_clean_high_looser_95low.py
+
+# SN10: Experimental
+script_12a_train_SN10_clean.py
+script_12c_train_SN10_clean_1v9.py
+script_12d_train_SN10_clean_high_looser_95low.py
+
+# SN10: Shuffled controls
+# TODO
+
+# Logistic Regression: Absolut
+# TODO
+
+# TODO
+
+```
+
+3. Compute the ID and OOD performances.
+
+```
+script_14_frozen_transfer_performance.py
+```
+
+4. Run the interpretability pipeline.
+
+```
+# Compute the energy contributions using Absolut
+script_16_compute_energy_contributions.py
+
+# Run the interpretability pipeline
+script_15_compute_attributions.py
+```
+
+## Analysis
+
+Once we have almost everything precomputed, we perform the analyses using notebooks.
+
+1. TODO
--- scripts/script_01_build_datasets.py
+++ scripts/script_01_build_datasets.py
@@ -24,16 +24,16 @@ dataset_path: Path = config.DATA_SLACK_1_RAW_DIR
 docopt_doc = """Build datasets.
 
 Usage:
-    script_01_build_datasets.py global
-    script_01_build_datasets.py add_farmhash_mod_10_to_global
-    script_01_build_datasets.py processed
-    script_01_build_datasets.py pairwise
-    script_01_build_datasets.py 1_vs_all
-    script_01_build_datasets.py download_absolut
-    script_01_build_datasets.py absolut_processed_multiclass
-    script_01_build_datasets.py absolut_processed_multilabel
+    # script_01_build_datasets.py global
+    # script_01_build_datasets.py add_farmhash_mod_10_to_global
+    # script_01_build_datasets.py processed
+    # script_01_build_datasets.py pairwise
+    # script_01_build_datasets.py 1_vs_all
+    # script_01_build_datasets.py absolut_processed_multiclass
+    # script_01_build_datasets.py absolut_processed_multilabel
+    script_01_build_datasets.py download_absolut  # 1
+    script_01_build_datasets.py miniabsolut  # 2
     script_01_build_datasets.py unzip_rawbindingsmurine
-    script_01_build_datasets.py miniabsolut
     script_01_build_datasets.py frozen_results
     script_01_build_datasets.py adapt_attributions_for_linear
     script_01_build_datasets.py add_freq_based_attributions
@@ -119,99 +119,99 @@ def process_downstream_and_save(out_dir, ags_open, df_wide):
 if __name__ == "__main__":
     arguments = docopt(docopt_doc, version="Naval Fate 2.0")
 
-    if arguments["global"]:
-        logging.info("Building the global dataset.")
-        dataset_name = dataset_path.name
-        df_global = utils.build_global_dataset(
-            dataset_path, remove_ag_slide_duplicates=True
-        )
-        config.DATA_SLACK_1_GLOBAL.parent.mkdir(exist_ok=True)
-        df_global.to_csv(config.DATA_SLACK_1_GLOBAL, sep="\t")
-    else:
-        df_global = pd.read_csv(
-            config.DATA_SLACK_1_GLOBAL, sep="\t", dtype={"Antigen": str}
-        )
+    # if arguments["global"]:
+    #     logging.info("Building the global dataset.")
+    #     dataset_name = dataset_path.name
+    #     df_global = utils.build_global_dataset(
+    #         dataset_path, remove_ag_slide_duplicates=True
+    #     )
+    #     config.DATA_SLACK_1_GLOBAL.parent.mkdir(exist_ok=True)
+    #     df_global.to_csv(config.DATA_SLACK_1_GLOBAL, sep="\t")
+    # else:
+    #     df_global = pd.read_csv(
+    #         config.DATA_SLACK_1_GLOBAL, sep="\t", dtype={"Antigen": str}
+    #     )
 
-    antigens = sorted(df_global["Antigen"].unique().tolist())
+    # antigens = sorted(df_global["Antigen"].unique().tolist())
 
-    if arguments["add_farmhash_mod_10_to_global"]:
-        df_global["Slide_farmhash_mod_10"] = df_global["Slide"].apply(
-            lambda x: preprocessing.farmhash_mod_10(x)
-        )
+    # if arguments["add_farmhash_mod_10_to_global"]:
+    #     df_global["Slide_farmhash_mod_10"] = df_global["Slide"].apply(
+    #         lambda x: preprocessing.farmhash_mod_10(x)
+    #     )
 
-        dir_ = config.DATA_SLACK_1_GLOBAL.parent
-        basename = config.DATA_SLACK_1_GLOBAL.stem
-        df_global.to_csv(dir_ / f"{basename}_farmhashed.tsv", sep="\t")
+    #     dir_ = config.DATA_SLACK_1_GLOBAL.parent
+    #     basename = config.DATA_SLACK_1_GLOBAL.stem
+    #     df_global.to_csv(dir_ / f"{basename}_farmhashed.tsv", sep="\t")
 
-    elif arguments["pairwise"]:
-        logging.info("Building pairwise datasets")
-        for ag1, ag2 in combinations(antigens, 2):
-            logging.info(f"Building pairwise dataset: {ag1} vs {ag2}")
-            datasets.generate_pairwise_dataframe(
-                df_global, ag1=ag1, ag2=ag2, read_if_exists=False
-            )
+    # if arguments["pairwise"]:
+    #     logging.info("Building pairwise datasets")
+    #     for ag1, ag2 in combinations(antigens, 2):
+    #         logging.info(f"Building pairwise dataset: {ag1} vs {ag2}")
+    #         datasets.generate_pairwise_dataframe(
+    #             df_global, ag1=ag1, ag2=ag2, read_if_exists=False
+    #         )
 
-    elif arguments["1_vs_all"]:
-        logging.info("Building 1_vs_all datasets")
-        for ag in antigens:
-            logging.info(f"Building 1_vs_all dataset: {ag}")
-            datasets.generate_1_vs_all_dataset(df_global, ag)
+    # elif arguments["1_vs_all"]:
+    #     logging.info("Building 1_vs_all datasets")
+    #     for ag in antigens:
+    #         logging.info(f"Building 1_vs_all dataset: {ag}")
+    #         datasets.generate_1_vs_all_dataset(df_global, ag)
 
-    elif arguments["processed"]:
-        logging.info("Building openset_exclusive dataset")
+    # elif arguments["processed"]:
+    #     logging.info("Building openset_exclusive dataset")
 
-        out_dir = Path(config.DATA_SLACK_1_PROCESSED_DIR)
-        out_dir.mkdir(exist_ok=True)
+    #     out_dir = Path(config.DATA_SLACK_1_PROCESSED_DIR)
+    #     out_dir.mkdir(exist_ok=True)
 
-        (
-            df_train_val,
-            df_test_closed_exclusive,
-            df_test_open_exclusive,
-        ) = preprocessing.openset_datasplit_from_global_stable(df_global)
+    #     (
+    #         df_train_val,
+    #         df_test_closed_exclusive,
+    #         df_test_open_exclusive,
+    #     ) = preprocessing.openset_datasplit_from_global_stable(df_global)
 
-        df_train_val.to_csv(out_dir / "df_train_val.tsv", sep="\t")
-        df_test_closed_exclusive.to_csv(
-            out_dir / "df_test_closed_exclusive.tsv", sep="\t"
-        )
-        df_test_open_exclusive.to_csv(out_dir / "df_test_open_exclusive.tsv", sep="\t")
+    #     df_train_val.to_csv(out_dir / "df_train_val.tsv", sep="\t")
+    #     df_test_closed_exclusive.to_csv(
+    #         out_dir / "df_test_closed_exclusive.tsv", sep="\t"
+    #     )
+    #     df_test_open_exclusive.to_csv(out_dir / "df_test_open_exclusive.tsv", sep="\t")
 
-        meta = {
-            "df_train_val__shape": df_train_val.shape,
-            "df_test_closed_exclusive__shape": df_test_closed_exclusive.shape,
-            "df_test_open_exclusive__shape": df_test_open_exclusive.shape,
-        }
-        with open(out_dir / "build_metadata.json", "w+") as fh:
-            json.dump(meta, fh)
+    #     meta = {
+    #         "df_train_val__shape": df_train_val.shape,
+    #         "df_test_closed_exclusive__shape": df_test_closed_exclusive.shape,
+    #         "df_test_open_exclusive__shape": df_test_open_exclusive.shape,
+    #     }
+    #     with open(out_dir / "build_metadata.json", "w+") as fh:
+    #         json.dump(meta, fh)
 
-    elif arguments["download_absolut"]:
+    if arguments["download_absolut"]:
         utils.download_absolut()
 
-    elif (
-        arguments["absolut_processed_multiclass"]
-        or arguments["absolut_processed_multilabel"]
-    ):
-        ds3 = datasets.AbsolutDataset3()
-        ags_closed, ags_open = get_closed_open_antigens(ds3)
+    # elif (
+    #     arguments["absolut_processed_multiclass"]
+    #     or arguments["absolut_processed_multilabel"]
+    # ):
+    #     ds3 = datasets.AbsolutDataset3()
+    #     ags_closed, ags_open = get_closed_open_antigens(ds3)
 
-        df_wide = ds3.df_wide
-        mask_c = (df_wide[ags_closed].sum(axis=1) >= 1) & (
-            df_wide[ags_open].sum(axis=1) == 0
-        )
-        mask_o = (df_wide[ags_closed].sum(axis=1) == 0) & (
-            df_wide[ags_open].sum(axis=1) >= 1
-        )
-        if arguments["absolut_processed_multiclass"]:
-            # Filter for unimodal binding and exclusive open set and closed sets.
-            mask_unimodal = df_wide.sum(axis=1) == 1
-            df_wide = df_wide.loc[
-                (mask_unimodal & mask_c) | (mask_unimodal & mask_o)
-            ].copy()
-            out_dir = config.DATA_ABSOLUT_PROCESSED_MULTICLASS_DIR
-        elif arguments["absolut_processed_multilabel"]:
-            df_wide = df_wide.loc[(mask_c) | (mask_o)].copy()
-            out_dir = config.DATA_ABSOLUT_PROCESSED_MULTILABEL_DIR
+    #     df_wide = ds3.df_wide
+    #     mask_c = (df_wide[ags_closed].sum(axis=1) >= 1) & (
+    #         df_wide[ags_open].sum(axis=1) == 0
+    #     )
+    #     mask_o = (df_wide[ags_closed].sum(axis=1) == 0) & (
+    #         df_wide[ags_open].sum(axis=1) >= 1
+    #     )
+    #     if arguments["absolut_processed_multiclass"]:
+    #         # Filter for unimodal binding and exclusive open set and closed sets.
+    #         mask_unimodal = df_wide.sum(axis=1) == 1
+    #         df_wide = df_wide.loc[
+    #             (mask_unimodal & mask_c) | (mask_unimodal & mask_o)
+    #         ].copy()
+    #         out_dir = config.DATA_ABSOLUT_PROCESSED_MULTICLASS_DIR
+    #     elif arguments["absolut_processed_multilabel"]:
+    #         df_wide = df_wide.loc[(mask_c) | (mask_o)].copy()
+    #         out_dir = config.DATA_ABSOLUT_PROCESSED_MULTILABEL_DIR
 
-        process_downstream_and_save(out_dir, ags_open, df_wide)  # type:ignore
+    #     process_downstream_and_save(out_dir, ags_open, df_wide)  # type:ignore
 
     elif arguments["unzip_rawbindingsmurine"]:
         input_dir = Path("data/Absolut/data/RawBindingsMurine")
@@ -230,111 +230,116 @@ if __name__ == "__main__":
         N_train = 15000
         N_test = 5000
 
-        MAKE_SPLITS = True
+        # MAKE_SPLITS = True
         # Get seed from arguments
         # if arguments["seed"]:
         #     seed = int(arguments["seed"])
         #     print(f"Using seed from arguments: {seed}")
-        seed = 4
+        # seed = 4
 
-        if MAKE_SPLITS:
-            base_p = Path(config.DATA_MINIABSOLUT_SPLITS) / f"MiniAbsolut_Seed{seed}"
-            base_p.mkdir(exist_ok=True, parents=False)
-        else:
-            base_p = config.DATA_MINIABSOLUT
-            base_p.mkdir(exist_ok=True, parents=False)
+        make_splits_l = [False, True, True, True, True, True, True]
+        seed_l = [None, 0, 1, 2, 3, 4, None]
 
-        # Load the Absolut binding data for the antigens in Mini-Absolut.
-        dfs = []
-        for ag in config.ANTIGENS:
-            # Load based on config.DATA_SLACK_1_RAWBINDINGS_PERCLASS_MURINE
-            df = utils.load_binding_per_ag(ag)
-            df["Antigen"] = ag
-            dfs.append(df)
-        df = pd.concat(dfs, axis=0)
+        for make_splits, seed in zip(make_splits_l, seed_l):
 
-        # Get mascotte
-        df_m = df.loc[df["Source"] == "mascotte"].copy()
+            if make_splits:
+                base_p = Path(config.DATA_MINIABSOLUT_SPLITS) / f"MiniAbsolut_Seed{seed}"
+                base_p.mkdir(exist_ok=True, parents=False)
+            else:
+                base_p = config.DATA_MINIABSOLUT
+                base_p.mkdir(exist_ok=True, parents=False)
 
-        # Get mascotte without duplicates
-        df_m_nodup = df_m.loc[~df_m["Slide"].duplicated(keep=False)].copy()
+            # Load the Absolut binding data for the antigens in Mini-Absolut.
+            dfs = []
+            for ag in config.ANTIGENS:
+                # Load based on config.DATA_SLACK_1_RAWBINDINGS_PERCLASS_MURINE
+                df = utils.load_binding_per_ag(ag)
+                df["Antigen"] = ag
+                dfs.append(df)
+            df = pd.concat(dfs, axis=0)
 
-        for ag in config.ANTIGENS:
-            print(f"Processing {ag}...")
+            # Get mascotte
+            df_m = df.loc[df["Source"] == "mascotte"].copy()
 
-            ag_dir = base_p / ag
-            ag_dir.mkdir(exist_ok=True, parents=False)
+            # Get mascotte without duplicates
+            df_m_nodup = df_m.loc[~df_m["Slide"].duplicated(keep=False)].copy()
 
-            # Get the mascotte data for the antigen.
-            df_ag = df_m_nodup.loc[df_m_nodup["Antigen"] == ag].copy()
-            df_ag = df_ag.loc[df_ag["Source"] == "mascotte"].copy()
-            df_ag = df_ag.sample(frac=1).reset_index(drop=True)  # shuffle
-            print(f"mascotte: {df_ag.shape}")
-            df_train, df_test, df_rest = split_to_train_test_rest_dfs(
-                N_train,
-                N_test,
-                df_ag,
-                random_state=seed,
-            )
-            save_train_test_rest(
-                "high", N_train, N_test, ag_dir, df_train, df_test, df_rest
-            )
+            for ag in config.ANTIGENS:
+                print(f"Processing {ag}...")
 
-            # Get looserX data for the antigen.
-            df_ag = df.loc[df["Antigen"] == ag].copy()
+                ag_dir = base_p / ag
+                ag_dir.mkdir(exist_ok=True, parents=False)
 
-            df_ag = df_ag.loc[
-                ~df_ag["Slide"].isin(
-                    df_m_nodup.loc[df_m_nodup["Antigen"] == ag]["Slide"]
+                # Get the mascotte data for the antigen.
+                df_ag = df_m_nodup.loc[df_m_nodup["Antigen"] == ag].copy()
+                df_ag = df_ag.loc[df_ag["Source"] == "mascotte"].copy()
+                df_ag = df_ag.sample(frac=1).reset_index(drop=True)  # shuffle
+                print(f"mascotte: {df_ag.shape}")
+                df_train, df_test, df_rest = split_to_train_test_rest_dfs(
+                    N_train,
+                    N_test,
+                    df_ag,
+                    random_state=seed,
                 )
-            ].copy()
-
-            df_ag.sort_values(
-                by="Energy", ascending=False, inplace=True
-            )  # make ascending == True ?
-            df_ag = df_ag.loc[~df_ag.duplicated(subset=["Slide"], keep="first")].copy()
-            df_ag = df_ag.loc[df_ag["Source"] == "looserX"].copy()
-            df_ag = df_ag.sample(frac=1).reset_index(drop=True)  # shuffle
-            print(f"looserX: {df_ag.shape}")
-            df_train, df_test, df_rest = split_to_train_test_rest_dfs(
-                N_train,
-                N_test,
-                df_ag,
-                random_state=seed,
-            )
-            save_train_test_rest(
-                "looserX", N_train, N_test, ag_dir, df_train, df_test, df_rest
-            )
-
-            # Get 95low data for the antigen.
-            df_ag = df.loc[df["Antigen"] == ag].copy()
-
-            df_ag = df_ag.loc[
-                ~df_ag["Slide"].isin(
-                    df_m_nodup.loc[df_m_nodup["Antigen"] == ag]["Slide"]
+                save_train_test_rest(
+                    "high", N_train, N_test, ag_dir, df_train, df_test, df_rest
                 )
-            ].copy()
 
-            df_ag.sort_values(
-                by="Energy", ascending=False, inplace=True
-            )  # make ascending == True ? Keep Slide with lowest energy?
-            df_ag = df_ag.loc[~df_ag.duplicated(subset=["Slide"], keep="first")].copy()
+                # Get looserX data for the antigen.
+                df_ag = df.loc[df["Antigen"] == ag].copy()
 
-            # Exclude 95low intersection with looserX.
-            energy_5p_cutoff = df_ag[df_ag["Source"] == "looserX"]["Energy"].max()
-            df_ag = df_ag.loc[df_ag["Energy"] >= energy_5p_cutoff].copy()  # make ">" ?
+                df_ag = df_ag.loc[
+                    ~df_ag["Slide"].isin(
+                        df_m_nodup.loc[df_m_nodup["Antigen"] == ag]["Slide"]
+                    )
+                ].copy()
 
-            df_ag = df_ag.sample(frac=1).reset_index(drop=True)  # shuffle
-            print(f"95low: {df_ag.shape}")
-            df_train, df_test, df_rest = split_to_train_test_rest_dfs(
-                N_train,
-                N_test,
-                df_ag,
-                random_state=seed,
-            )
-            save_train_test_rest(
-                "95low", N_train, N_test, ag_dir, df_train, df_test, df_rest
-            )
+                df_ag.sort_values(
+                    by="Energy", ascending=False, inplace=True
+                )  # make ascending == True ?
+                df_ag = df_ag.loc[~df_ag.duplicated(subset=["Slide"], keep="first")].copy()
+                df_ag = df_ag.loc[df_ag["Source"] == "looserX"].copy()
+                df_ag = df_ag.sample(frac=1).reset_index(drop=True)  # shuffle
+                print(f"looserX: {df_ag.shape}")
+                df_train, df_test, df_rest = split_to_train_test_rest_dfs(
+                    N_train,
+                    N_test,
+                    df_ag,
+                    random_state=seed,
+                )
+                save_train_test_rest(
+                    "looserX", N_train, N_test, ag_dir, df_train, df_test, df_rest
+                )
+
+                # Get 95low data for the antigen.
+                df_ag = df.loc[df["Antigen"] == ag].copy()
+
+                df_ag = df_ag.loc[
+                    ~df_ag["Slide"].isin(
+                        df_m_nodup.loc[df_m_nodup["Antigen"] == ag]["Slide"]
+                    )
+                ].copy()
+
+                df_ag.sort_values(
+                    by="Energy", ascending=False, inplace=True
+                )  # make ascending == True ? Keep Slide with lowest energy?
+                df_ag = df_ag.loc[~df_ag.duplicated(subset=["Slide"], keep="first")].copy()
+
+                # Exclude 95low intersection with looserX.
+                energy_5p_cutoff = df_ag[df_ag["Source"] == "looserX"]["Energy"].max()
+                df_ag = df_ag.loc[df_ag["Energy"] >= energy_5p_cutoff].copy()  # make ">" ?
+
+                df_ag = df_ag.sample(frac=1).reset_index(drop=True)  # shuffle
+                print(f"95low: {df_ag.shape}")
+                df_train, df_test, df_rest = split_to_train_test_rest_dfs(
+                    N_train,
+                    N_test,
+                    df_ag,
+                    random_state=seed,
+                )
+                save_train_test_rest(
+                    "95low", N_train, N_test, ag_dir, df_train, df_test, df_rest
+                )
 
     elif arguments["frozen_results"]:
         # Save the frozen results in a convenient format for sharing and further analysis.
